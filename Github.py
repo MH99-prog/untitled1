6c8723ddad44805b8dacb889b7906164007df513
Muhammad Haleem <haleem.muhammad@outlook.com>
2018-01-15 00:07:11 +0500

    My favourite sport

--- Github.py
+++ Github.py
@@ -3,3 +3,9 @@ name = 'Racker'
 print ("My name is "+name+".")
 age = 13
 print("I am " +str(age)+" years old.")
+a = input("What is your favourite game?")
+while a:
+
+    print(a)
+    break
+print("My favourite game is football!")
